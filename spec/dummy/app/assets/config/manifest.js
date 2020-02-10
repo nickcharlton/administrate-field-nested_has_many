a7bbd5278a6474eb0ab2445b56933b0f42920e54
Sebastien Dubois <sedubois@users.noreply.github.com>
2020-02-10 18:34:24 +0100

    Explitly import administrate and field JS/CSS

--- spec/dummy/app/assets/config/manifest.js
+++ spec/dummy/app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
+//= link administrate/application.js
+//= link administrate-field-nested_has_many/application.js
 //= link_directory ../stylesheets .css
+//= link administrate/application.css
+//= link administrate-field-nested_has_many/application.css
