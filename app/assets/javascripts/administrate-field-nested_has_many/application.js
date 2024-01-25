526b7c7f962c6376b83fd05332aa7bb149f66583
jubilee2 <jubilee2@gmail.com>
2024-01-25 14:30:32 -0600

    Add Selectize support for the select boxes (#64)

--- app/assets/javascripts/administrate-field-nested_has_many/application.js
+++ app/assets/javascripts/administrate-field-nested_has_many/application.js
@@ -1,1 +1,7 @@
 //= require cocoon
+$(document).on('ready page:load turbolinks:load turbo:load cocoon:after-insert', function() {
+  $('.field-unit--select select').selectize({});
+  $('.field-unit--belongs-to select').selectize({});
+  $(".field-unit--has-many select").selectize({});
+  $('.field-unit--polymorphic select').selectize({});
+});
